72d66a150379c818073a5f4522d6784583c30b74
Talor <talor@replit.com>
2025-08-08 17:40:31 -0700

    fix drizzle config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -6,7 +6,7 @@ export default {
   dialect: "sqlite",
   driver: "d1-http",
   dbCredentials: {
-    databaseId: "your-database-id",
+    databaseId: "bb1dffa7-ec33-4f38-9ac1-6507926dc3fc",
     accountId: process.env.CLOUDFLARE_ACCOUNT_ID!,
     token: process.env.CLOUDFLARE_TOKEN!,
   },
